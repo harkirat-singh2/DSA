5b6f32c4c5857bc76b2405b1b735e34c7d93fb84
Harkirat <harkiratsingh20026@gmail.com>
2026-02-20 10:23:07 +0530

    Added recursion and array DSA programs

--- src/patterns.java
+++ src/patterns.java
@@ -18,13 +18,20 @@ public class patterns {
     public static void numberSquare(int n){
         for (int i = 1; i <= n; i++) { // Kitni lines Hogi
             for (int j = 1; j <= n; j++) {  //Har lines mein kitna print hoga
-                System.out.print(j+" ");
+                System.out.print((j)+" ");
             }
             System.out.println();
         }
     }
-
+    public static void alphabetSquare(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= n; j++) {  //Har lines mein kitna print hoga
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        numberSquare(4);
+        alphabetSquare(6);
     }
 }

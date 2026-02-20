eefc7d3b2e8af5848ec84b3456305dae008eb400
Harkirat <harkiratsingh20026@gmail.com>
2026-02-20 09:34:40 +0530

    Added recursion and array DSA programs

--- src/loops.java
+++ src/loops.java
@@ -53,7 +53,34 @@ public class loops {
             System.out.print(ch+" "+(int)ch);
         }
     }
-    public static void main(String[] args) {
 
+    public static int countDigits(int n){
+        int count =0;
+        while (n!=0){
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+    public static int sumOfDigits(int n){
+        int sum =0;
+        while (n!=0){
+            sum+= n%10;
+            n/=10;
+        }
+        return sum;
+    }
+
+    public static int revOfNum(int n){
+        int rev =0;
+        while (n!=0){
+            rev= (n%10 )+ (rev*10);
+            n/=10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(revOfNum(61234));
     }
 }

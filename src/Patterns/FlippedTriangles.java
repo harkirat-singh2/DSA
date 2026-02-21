cbe5f9766b9e0dad38b6ff5281b48f2e29437985
Harkirat <harkiratsingh20026@gmail.com>
2026-02-21 07:06:21 +0530

    Added recursion and array DSA programs

--- src/Patterns/FlippedTriangles.java
+++ src/Patterns/FlippedTriangles.java
@@ -9,6 +9,16 @@ public class FlippedTriangles {
             System.out.println();
         }
     }
+    public static void jugaadflippedtriangle(int n){
+        int a = n;
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= a; j++) {  //Har lines mein kitna print hoga
+                System.out.print("* ");
+            }
+            a--;
+            System.out.println();
+        }
+    }
     public static void straightNumericflippedtriangle(int n){
         for (int i = 1; i <= n; i++) { // Kitni lines Hogi
             for (int j = 1; j <= n-i+1; j++) {  //Har lines mein kitna print hoga
@@ -21,6 +31,14 @@ public class FlippedTriangles {
         for (int i = 1; i <= n; i++) { // Kitni lines Hogi
             for (int j = 1; j <= n-i+1; j++) {  //Har lines mein kitna print hoga
                 System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void sameAlphabetstraightAlphabeticflippedtriangle(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= n-i+1; j++) {  //Har lines mein kitna print hoga
+                System.out.print((char)(i+64)+" ");
             }
             System.out.println();
         }
@@ -43,6 +61,6 @@ public class FlippedTriangles {
     }
 
     public static void main(String[] args) {
-    straightAlphabeticflippedtriangle(5);
+        sameAlphabetstraightAlphabeticflippedtriangle(5);
     }
 }

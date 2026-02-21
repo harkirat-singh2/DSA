e9662337d5bbe9bbc6d99452aaeb469ac70b9aea
Harkirat <harkiratsingh20026@gmail.com>
2026-02-21 08:23:14 +0530

    Added recursion and array DSA programs

--- src/Patterns/SpecialPatterns.java
+++ src/Patterns/SpecialPatterns.java
@@ -40,8 +40,43 @@ public class SpecialPatterns {
             System.out.println();
         }
     }
+    public static void floyddTriangle(int n){
+        int a = 1;
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= i; j++) {  //Har lines mein kitna print hoga
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+    public static void binaryTriangle(int n){
+        int b= 0;
+        int a = 1;
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= i; j++) {  //Har lines mein kitna print hoga
+                if ((i+j)%2!=0)System.out.print(b+" ");
+                if ((i+j)%2==0)System.out.print(a+" ");
 
+            }
+            System.out.println();
+        }
+    }
+    public static void oddTriangle(int n) {
+
+        for (int i = 1; i <= n; i++) {   // rows
+
+            int num = 1;   // reset to 1 every row
+
+            for (int j = 1; j <= i; j++) {  // columns
+                System.out.print(num + " ");
+                num += 2;   // next odd number
+            }
+
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-starCross(7);
+    oddTriangle(7);
     }
 }

a9846fe1f306e8e671695c75f358a864ddaa39b2
Harkirat <harkiratsingh20026@gmail.com>
2026-02-21 06:58:32 +0530

    Added recursion and array DSA programs

--- src/Patterns/FlippedTriangles.java
+++ src/Patterns/FlippedTriangles.java
@@ -9,7 +9,40 @@ public class FlippedTriangles {
             System.out.println();
         }
     }
+    public static void straightNumericflippedtriangle(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= n-i+1; j++) {  //Har lines mein kitna print hoga
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void straightAlphabeticflippedtriangle(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = 1; j <= n-i+1; j++) {  //Har lines mein kitna print hoga
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void diffMethodflippedtriangle(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = n; j >= i; j--) {  //Har lines mein kitna print hoga
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void diffMethodNumericflippedtriangle(int n){
+        for (int i = 1; i <= n; i++) { // Kitni lines Hogi
+            for (int j = n; j >= i; j--) {  //Har lines mein kitna print hoga
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-flippedtriangle(5);
+    straightAlphabeticflippedtriangle(5);
     }
 }

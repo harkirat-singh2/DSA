139f94813f9a10830a65ccb027970621ce2a337f
Harkirat <harkiratsingh20026@gmail.com>
2026-02-25 22:11:20 +0530

    Added recursion and array DSA programs

---
+++ src/SortingAlgorithms/Selection.java
@@ -0,0 +1,28 @@
+package SortingAlgorithms;
+
+public class Selection {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {          // passes
+            int min = Integer.MAX_VALUE;
+            int pos=-1;
+            for (int j = 0; j < n - i -1 ; j++) {  // comparisons
+                if (min > arr[j]) {         // correct indices
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void print (int[] arr){
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={1,2,8,0,-4,0,6,5,0,3,-2,-9,4};
+    }
+}
--- src/Strings/Questions.java
+++ src/Strings/Questions.java
@@ -25,7 +25,6 @@ public class Questions {
                     ans=s.charAt(maxFreq);
                 }
             }
-
         }
         return ans;
     }

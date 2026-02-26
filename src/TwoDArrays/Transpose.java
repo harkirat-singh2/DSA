f0dcb2e9c897f6223f440486817012217971e40b
Harkirat <harkiratsingh20026@gmail.com>
2026-02-26 21:19:17 +0530

    Added recursion and array DSA programs

--- src/TwoDArrays/Transpose.java
+++ src/TwoDArrays/Transpose.java
@@ -44,6 +44,15 @@ public class Transpose {
             }
         }
     }
+    public static void printTranspose(int[][] arr, int n, int m) {
+
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
         for (int i = 0; i < arr.length; i++) {

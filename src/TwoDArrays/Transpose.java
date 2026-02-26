cc9b35448657d83471bc70af85507438ff23d82f
Harkirat <harkiratsingh20026@gmail.com>
2026-02-26 21:18:40 +0530

    Added recursion and array DSA programs

--- src/TwoDArrays/Transpose.java
+++ src/TwoDArrays/Transpose.java
@@ -8,6 +8,42 @@ public class Transpose {
             }
         }
     }
+    public static void spiralPrint(int[][] arr, int n, int m) {
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Left → Right
+            for (int i = left; i <= right; i++) {
+                System.out.print(arr[top][i] + " ");
+            }
+            top++;
+
+            // Top → Bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(arr[i][right] + " ");
+            }
+            right--;
+
+            // Right → Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(arr[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Bottom → Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(arr[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
         for (int i = 0; i < arr.length; i++) {

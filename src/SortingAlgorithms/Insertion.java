2827e061fbaa3d64f81d6be7e34f4653a11bdc6a
Harkirat <harkiratsingh20026@gmail.com>
2026-02-27 21:43:45 +0530

    Added recursion and array DSA programs

--- src/SortingAlgorithms/Insertion.java
+++ src/SortingAlgorithms/Insertion.java
@@ -1,7 +1,26 @@
 package SortingAlgorithms;
 
 public class Insertion {
-    public static void main(String[] args) {
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+           int j = i;
+            while (j>0 && arr[j]<arr[j-1]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
 
+            }
+        }
+    }
+    public static void print(int[] arr){
+        for (int el : arr){
+            System.out.print(el+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr ={1,2,8,0,-4,0,6,5,0,3,-2,-9,4};
+        insertionSort(arr);
+        print(arr);
     }
 }

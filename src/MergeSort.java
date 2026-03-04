523dce2f3e6ca14f0298c530e40d6930686df60c
Harkirat <harkiratsingh20026@gmail.com>
2026-03-04 21:27:39 +0530

    Added recursion and array DSA programs

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,63 +1,67 @@
 public class MergeSort {
-    public static int[] mergingTwoSortedArr(int[] arr1 , int[] arr2){
-        int size = arr1.length+arr2.length;
+    public static int[] mergingTwoSortedArr(int[] arr1, int[] arr2) {
+        int size = arr1.length + arr2.length;
         int[] arr = new int[(size)];
         int i = 0, j = 0, k = 0;
-        while (i<arr1.length && j<arr2.length){
-            if(arr1[i] < arr2[j]){
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
                 arr[k] = arr1[i];
                 i++;
-            }else{
+            } else {
                 arr[k] = arr2[j];
                 j++;
             }
             k++;
         }
-        while(i < arr1.length){
+        while (i < arr1.length) {
             arr[k] = arr1[i];
             i++;
             k++;
         }
-        while(j < arr2.length){
+        while (j < arr2.length) {
             arr[k] = arr2[j];
             j++;
             k++;
         }
         return arr;
     }
-    public static void  print(int[] arr){
-        for (int el : arr){
-            System.out.print(el+" ");
-        }
-    }
-    public static void main(String[] args) {
-        int[] arr = {1,5,6,8,7,9,3,4};
-        int[] a = {1,2,5,7,10};
-        int[] b = {3,4,6,8,9};
-        int[] c = mergingTwoSortedArr(a,b);
-        mergeSort(arr);
-        print (arr);
-    }
 
     private static void mergeSort(int[] arr) {
-        if(arr.length <= 1) return;
+        if (arr.length <= 1) return;
         int n = arr.length;
         //Create 2 arrays of sizen/2 each
-        int[] a = new int[n/2];
-        int[] b = new int[n-(n/2)];
+        int[] a = new int[n / 2];
+        int[] b = new int[n - (n / 2)];
         // Step 2 : Copy-paste arr into a and b
         int idx = 0; // idx travel karega arr pe
-        for(int i=0;i<a.length; i++)
+        for (int i = 0; i < a.length; i++)
             a[i] = arr[idx++];
-        for(int i=0;i<b.length; i++)
+        for (int i = 0; i < b.length; i++)
             b[i] = arr[idx++];
         mergeSort(a);
         mergeSort(b);
         // Merge a and b into arr
-        int[] merged = mergingTwoSortedArr(a,b);
+        int[] merged = mergingTwoSortedArr(a, b);
 
         System.arraycopy(merged, 0, arr, 0, arr.length);
 
     }
+
+
+    public static void print(int[] arr) {
+        for (int el : arr) {
+            System.out.print(el + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 6, 8, 7, 9, 3, 4};
+        int[] a = {1, 2, 5, 7, 10};
+        int[] b = {3, 4, 6, 8, 9};
+        int[] c = mergingTwoSortedArr(a, b);
+        mergeSort(arr);
+        print(arr);
+    }
+
 
 }

0f1d77d2ff0f6749bd013cb7097c41d84fd2238d
Harkirat <harkiratsingh20026@gmail.com>
2026-03-05 11:48:09 +0530

    Added recursion and array DSA programs

--- src/LinkedList/BasicsOFLinkedList.java
+++ src/LinkedList/BasicsOFLinkedList.java
@@ -19,13 +19,26 @@ public class BasicsOFLinkedList {
     public static void displayRec(Node head){
         Node temp = head;
         if (temp==null) return;
-        System.out.print(temp.val);
+        System.out.print(temp.val+" ");
         displayRec(temp.next);
 
         System.out.println();
     }
+    public static int get(Node head ,int idx){
+        Node temp = head;
+        for (int i = 0; i <= idx; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
+    }
     public static void main(String[] args) {
         Node a = new Node(5);
-        displayRec(a);
+        Node b = new Node(4);
+        Node c = new Node(3);
+        Node d = new Node(9);
+        Node e = new Node(8);
+        a.next = b; b.next = c;
+        c.next = d; d.next = e;
+        System.out.println(get(a,2));
     }
 }

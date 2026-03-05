ede2b812603088fa1566bd9b0277aa8878981c55
Harkirat <harkiratsingh20026@gmail.com>
2026-03-05 11:22:15 +0530

    Added recursion and array DSA programs

---
+++ src/LinkedList/BasicsOFLinkedList.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+class Node{
+    int val;
+    Node next;
+}
+public class BasicsOFLinkedList {
+    public static void main(String[] args) {
+        Node a = new Node();
+    }
+}

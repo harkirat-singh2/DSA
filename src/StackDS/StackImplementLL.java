49e153f7342129d7684a523872beecca6e0a77a2
Harkirat <harkiratsingh20026@gmail.com>
2026-03-17 09:55:37 +0530

    Added recursion and array DSA programs

--- src/StackDS/StackImplementLL.java
+++ src/StackDS/StackImplementLL.java
@@ -12,13 +12,23 @@ class MyStack{
     int length;
 
     int peek(){
+        if (head==null){
+            System.out.println("Empty Stack");
+            return -1;
+        }
         return head.val;
     }
     int pop(){
-        head  = head.next;
-        length--;
-        return head.val;
-
+        if (head==null){
+            System.out.println("Empty Stack");
+            return -1;
+        }
+        else{
+            int x = head.val;
+            head = head.next;
+            length--;
+            return x;
+        }
     }
     void push(int el){
        Node temp = new Node(el);
@@ -26,15 +36,32 @@ class MyStack{
        else {
            temp.next = head;
            head = temp;
-           length++;
        }
+        length++;
     }
     int siz(){
         return length;
+    }
+    void print(){
+       Node temp = head;
+       while (temp!=null){
+           System.out.print(temp.val+" ");
+           temp = temp.next;
+       }
     }
 }
 public class StackImplementLL {
     public static void main(String[] args) {
         MyStack st  = new MyStack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.print();
+        System.out.println();
+        st.push(50);
+        st.print();
+        st.pop();
+
     }
 }

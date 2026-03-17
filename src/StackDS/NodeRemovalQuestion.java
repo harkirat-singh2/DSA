c8c5e04f09e8246a281c150d647093a792acaaf2
Harkirat <harkiratsingh20026@gmail.com>
2026-03-17 12:21:02 +0530

    Added recursion and array DSA programs

---
+++ src/StackDS/NodeRemovalQuestion.java
@@ -0,0 +1,41 @@
+package StackDS;
+
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class NodeRemovalQuestion {
+
+    public ListNode removeNodes(ListNode head) {
+
+        Stack<ListNode> st = new Stack<>();
+        ListNode temp = head;
+
+        // Step 1: Traverse and maintain decreasing stack
+        while (temp != null) {
+            while (!st.isEmpty() && st.peek().val < temp.val) {
+                st.pop();
+            }
+            st.push(temp);
+            temp = temp.next;
+        }
+
+        // Step 2: Rebuild list
+        ListNode newHead = null;
+
+        while (!st.isEmpty()) {
+            ListNode node = st.pop();
+            node.next = newHead;
+            newHead = node;
+        }
+
+        return newHead;
+    }
+}

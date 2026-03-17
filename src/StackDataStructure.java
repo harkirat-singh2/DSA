645353f74dc2dfb296d3dc1f5d963a2773ba8258
Harkirat <harkiratsingh20026@gmail.com>
2026-03-17 08:22:29 +0530

    Added recursion and array DSA programs

---
+++ src/StackDataStructure.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class StackDataStructure {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+        st.push("Khushi");
+        st.push("Aryan");
+        st.push("Raghav");
+        st.push("Puneet");
+        st.push("Muskan");
+        st.push("Rohit");
+        System.out.println(st);
+    }
+}

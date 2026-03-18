8c64f343ecc51ffa790773e8b880bb7658303d29
Harkirat <harkiratsingh20026@gmail.com>
2026-03-18 17:19:48 +0530

    Added recursion and array DSA programs

--- src/ifElseQuestion.java
+++ src/ifElseQuestion.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ifElseQuestion {
-    public static void main(String[] args) {
+    static void ifElse(){
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
         double cgpa = sc.nextDouble();
@@ -35,5 +35,19 @@ public class ifElseQuestion {
         else {
             System.out.println("Rejected");
         }
+    }
+
+     static void pattern(int n){
+        int count = 1;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count +" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
     }
 }

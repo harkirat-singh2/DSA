7300ae40f14809901a1d3cf794f4f23fe17b6d4a
Harkirat <harkiratsingh20026@gmail.com>
2026-03-21 14:59:31 +0530

    Added recursion and array DSA programs

--- src/ifElseQuestion.java
+++ src/ifElseQuestion.java
@@ -39,6 +39,7 @@ public class ifElseQuestion {
 
      static void pattern(int n){
         int count = 1;
+
         for (int i = 1; i <= n ; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(count +" ");

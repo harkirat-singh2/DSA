ec4a6d618de1ac8dbec19df30f97021128f2f050
Harkirat <harkiratsingh20026@gmail.com>
2026-03-22 17:04:00 +0530

    Added recursion and array DSA programs

---
+++ src/QueuesDataStructure/QueueArray.java
@@ -0,0 +1,17 @@
+package QueuesDataStructure;
+
+import java.util.Scanner;
+
+public class QueueArray {
+    int f , r , size;
+    int add(int val){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+    }
+}

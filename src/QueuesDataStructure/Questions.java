37f19e6742d2198cfff121418fafdb97b2bfc820
Harkirat <harkiratsingh20026@gmail.com>
2026-03-23 12:02:37 +0530

    Added recursion and array DSA programs

--- src/QueuesDataStructure/Questions.java
+++ src/QueuesDataStructure/Questions.java
@@ -2,9 +2,10 @@ package QueuesDataStructure;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Questions {
-    public Queue<Integer> rearrangeQueue(Queue<Integer> q) {
+    public Queue<Integer> rearrangeQueueByQueue(Queue<Integer> q) {
         Queue<Integer> p = new LinkedList<>();
         int n = q.size();
         for (int i = 0; i <n/2 ; i++) {
@@ -13,6 +14,31 @@ public class Questions {
         while (!p.isEmpty()) {
             q.add(p.remove());   // from first half
             q.add(q.remove());   // from second half
+        }
+        return q;
+    }
+
+    public Queue<Integer> rearrangeQueueByStack(Queue<Integer> q) {
+        Stack<Integer> p = new Stack<>();
+        int n = q.size();
+        for (int i = 0; i <n/2 ; i++) {
+            p.push(q.remove());
+        }
+        while (!p.isEmpty()){
+            q.add(p.pop());
+        }
+        for (int i = 0; i <n/2 ; i++) {
+            p.push(q.remove());
+        }
+        while (!p.isEmpty()){
+            q.add(p.pop());
+            q.add(q.remove());
+        }
+        while (!q.isEmpty()){
+            p.push(q.remove());
+        }
+        while (!p.isEmpty()){
+            q.add(p.pop());
         }
         return q;
     }

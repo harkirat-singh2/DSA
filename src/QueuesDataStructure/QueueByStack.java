b357407ff9fb1b55d0283c8765f5d5f82ac05c70
Harkirat <harkiratsingh20026@gmail.com>
2026-03-23 11:01:39 +0530

    Added recursion and array DSA programs

--- src/QueuesDataStructure/QueueByStack.java
+++ src/QueuesDataStructure/QueueByStack.java
@@ -48,6 +48,42 @@ class MyQueuePushEfficient {
 class MyQueuePopEfficient{
     Stack<Integer> st = new Stack<>();
     Stack<Integer> helper = new Stack<>();
+
+    public void push(int x) {
+        st.push(x);
+    }
+
+    public int pop() {
+        while (st.size() > 1) {
+            helper.push(st.pop());
+        }
+
+        int el = st.pop();
+
+        while (!helper.isEmpty()) {
+            st.push(helper.pop());
+        }
+
+        return el;
+    }
+
+    public int peek() {
+        while (st.size() > 1) {
+            helper.push(st.pop());
+        }
+
+        int el = st.peek();
+
+        while (!helper.isEmpty()) {
+            st.push(helper.pop());
+        }
+
+        return el;
+    }
+
+    public boolean empty() {
+        return st.isEmpty();
+    }
 }
 
 public class QueueByStack {

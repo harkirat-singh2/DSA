68aa44946c806891897bd83e528c951193fb8fb3
Harkirat <harkiratsingh20026@gmail.com>
2026-03-23 11:17:08 +0530

    Added recursion and array DSA programs

---
+++ src/QueuesDataStructure/StackByQueue.java
@@ -0,0 +1,34 @@
+package QueuesDataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack{
+    Queue<Integer> q = new LinkedList<>();
+    int n = q.size();
+
+    void push(int x){
+        q.add(x);
+    }
+    int pop(){
+        for (int i = 0; i < n-1; i++) {
+            q.add(q.remove());
+        }
+        return q.remove();
+    }
+    int peek(){
+        for (int i = 0; i < n-1; i++) {
+            q.add(q.remove());
+        }
+        return q.peek();
+    }
+    boolean isEmpty(){
+        return (q.isEmpty());
+    }
+}
+
+public class StackByQueue {
+    public static void main(String[] args) {
+
+    }
+}

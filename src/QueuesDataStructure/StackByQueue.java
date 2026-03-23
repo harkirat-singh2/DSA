e3f43f8e3a64a078fddc5a9cb84cb533254f4178
Harkirat <harkiratsingh20026@gmail.com>
2026-03-23 11:22:11 +0530

    Added recursion and array DSA programs

--- src/QueuesDataStructure/StackByQueue.java
+++ src/QueuesDataStructure/StackByQueue.java
@@ -3,20 +3,59 @@ package QueuesDataStructure;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class MyStack{
+import java.util.*;
+
+class MyStackPushEfficient {
     Queue<Integer> q = new LinkedList<>();
-    int n = q.size();
+
+    void push(int x) {
+        q.add(x);
+    }
+
+    int pop() {
+        int n = q.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            q.add(q.remove());
+        }
+
+        return q.remove();
+    }
+
+    int peek() {
+        int n = q.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            q.add(q.remove());
+        }
+
+        int x = q.peek();
+        q.add(q.remove());   // restore order
+
+        return x;
+    }
+
+    boolean isEmpty() {
+        return q.isEmpty();
+    }
+}
+
+class MyStackPopEfficient{
+    Queue<Integer> q = new LinkedList<>();
+
 
     void push(int x){
         q.add(x);
     }
     int pop(){
+        int n = q.size();
         for (int i = 0; i < n; i++) {
             q.add(q.remove());
         }
         return q.remove();
     }
     int peek(){
+        int n = q.size();
         for (int i = 0; i < n; i++) {
             q.add(q.remove());
         }
@@ -28,6 +67,7 @@ class MyStack{
         return (q.isEmpty());
     }
 }
+
 
 public class StackByQueue {
     public static void main(String[] args) {

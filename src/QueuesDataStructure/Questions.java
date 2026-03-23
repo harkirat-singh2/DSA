b43fe28396da2de9cd2fef429841a8ee8f2fc12c
Harkirat <harkiratsingh20026@gmail.com>
2026-03-23 11:38:13 +0530

    Added recursion and array DSA programs

--- src/QueuesDataStructure/Questions.java
+++ src/QueuesDataStructure/Questions.java
@@ -1,6 +1,22 @@
 package QueuesDataStructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Questions {
+    public void rearrangeQueue(Queue<Integer> q) {
+        Queue<Integer> p = new LinkedList<>();
+        int n = q.size();
+        for (int i = 0; i <n/2 ; i++) {
+            p.add(q.remove());
+        }
+        for (int i = n/2; i <n ; i++) {
+            q.add(p.remove());
+            q.add(q.remove());
+        }
+
+    }
+
     public static void main(String[] args) {
 
     }

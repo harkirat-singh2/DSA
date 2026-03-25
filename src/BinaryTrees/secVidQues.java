f45af8be37a23f342c3c8c0927eed85fd626a9c2
Harkirat <harkiratsingh20026@gmail.com>
2026-03-25 11:24:26 +0530

    Added recursion and array DSA programs

--- src/BinaryTrees/secVidQues.java
+++ src/BinaryTrees/secVidQues.java
@@ -65,6 +65,15 @@ class Solutions {
         view(root.left,level+1,ans);
     }
 
+    public Node lowestCommonAncestor(Node root, Node p, Node q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        Node left = lowestCommonAncestor(root.left, p, q);
+        Node right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return (left != null) ? left : right;
+    }
+
 }
 
 public class secVidQues {

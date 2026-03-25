91beb11be4181a396510403cd5866177a2a30938
Harkirat <harkiratsingh20026@gmail.com>
2026-03-25 15:08:27 +0530

    Added recursion and array DSA programs

---
+++ src/BST/BinarySearchTrees.java
@@ -0,0 +1,31 @@
+package BST;
+
+import com.sun.source.tree.Tree;
+
+class TreeNode{
+    TreeNode left, right;
+    int val;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+class Pair{
+    int min , max;
+    Pair(int min , int max){
+        this.min = min;
+        this.max = max;
+    }
+}
+
+public class BinarySearchTrees {
+
+    Pair minAndMaxBST(TreeNode root){
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

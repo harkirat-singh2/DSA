b71569a9fe48e91636c10176473026e071cba49d
Harkirat <harkiratsingh20026@gmail.com>
2026-03-26 09:41:54 +0530

    Added recursion and array DSA programs

--- src/BST/BinarySearchTrees.java
+++ src/BST/BinarySearchTrees.java
@@ -115,6 +115,14 @@ public class BinarySearchTrees {
 
     }
 
+     TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q);
+        if (p.val>root.val &&  q.val>root.val)return lowestCommonAncestor(root.right,p,q);
+        return root;
+
+    }
+
     public static void main(String[] args) {
         TreeNode a = new TreeNode(1);
         TreeNode b = new TreeNode(0);

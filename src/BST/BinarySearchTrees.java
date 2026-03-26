39c8af3a94c00772c20544333a61254f50de770e
Harkirat <harkiratsingh20026@gmail.com>
2026-03-26 10:42:43 +0530

    Added recursion and array DSA programs

--- src/BST/BinarySearchTrees.java
+++ src/BST/BinarySearchTrees.java
@@ -135,6 +135,19 @@ public class BinarySearchTrees {
         return root;
     }
 
+    public TreeNode sortedArrayToBST(int[] arr) {
+        int n = arr.length;
+        return convert(arr,0,n-1);
+    }
+    public TreeNode convert(int[] arr,int low , int high) {
+        if(low>high) return null;
+        int mid = (low+high)/2;
+        TreeNode root = new TreeNode(arr[mid]);
+        root.left = convert(arr,low,mid-1);
+        root.right = convert(arr,mid+1,high);
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode a = new TreeNode(1);
         TreeNode b = new TreeNode(0);

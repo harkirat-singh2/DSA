607b3d0a1d51828f7ec6339c8e213c7b14b2ad1a
Harkirat <harkiratsingh20026@gmail.com>
2026-03-28 11:08:18 +0530

    Added recursion and array DSA programs

--- src/MapsAndSets/HashmapImplement.java
+++ src/MapsAndSets/HashmapImplement.java
@@ -99,6 +99,21 @@ public class HashmapImplement {
         return true;
     }
 
+    int equalPairs(String s){
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        int pairs = 0;
+        for (char ch : map.keySet()){
+            int freq = map.get(ch);
+            pairs+= freq*freq;
+        }
+        return pairs;
+    }
+
+
     public static void main(String[] args) {
         HashmapImplement hmap = new HashmapImplement();
         String s = "aabbbcc";

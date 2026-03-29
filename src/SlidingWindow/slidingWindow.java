1c253c74c345eeb043ecc2805b40a61d9a19c44c
Harkirat <harkiratsingh20026@gmail.com>
2026-03-29 10:01:34 +0530

    Added recursion and array DSA programs

--- src/SlidingWindow/slidingWindow.java
+++ src/SlidingWindow/slidingWindow.java
@@ -1,6 +1,20 @@
 package SlidingWindow;
 
 public class slidingWindow {
+
+    int maxSumOfSubarray(int[] arr, int k){
+        int n = arr.length;
+        int maxSum = 0;
+        for (int i = 0; i < n-k+1; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k - 1; j++) {
+                sum+=arr[j];
+            }
+            maxSum = Math.max(maxSum,sum);
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
 
     }

cfa387e4590db02c5a9fd77ecef1d2837ded14f5
Harkirat <harkiratsingh20026@gmail.com>
2026-04-01 11:04:17 +0530

    Added recursion and array DSA programs

--- src/ArrayStart/arrayss.java
+++ src/ArrayStart/arrayss.java
@@ -117,18 +117,14 @@ public class arrayss {
     }
     public static void reverseArray(int[] arr){
         int n = arr.length;
-//        int i = 0;
-//        int j = arr.length-1;
-//        while(i<j){
-//            int temp = arr[i];
-//            arr[i]=arr[j];
-//            arr[j]=temp;
-//            i++;
-//            j--;
-        for (int i = 0; i < n / 2; i++) {
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
             int temp = arr[i];
-           arr[i]=arr[n-i-1];
-           arr[n-i-1]=temp;
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
         }
         for(int el : arr) System.out.print(el +" ");
     }

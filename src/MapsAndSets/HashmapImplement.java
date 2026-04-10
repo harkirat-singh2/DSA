6f31277724d715d655d290800795147d5e1ae77d
Harkirat <harkiratsingh20026@gmail.com>
2026-04-10 09:42:40 +0530

    Added recursion and array DSA programs

--- src/MapsAndSets/HashmapImplement.java
+++ src/MapsAndSets/HashmapImplement.java
@@ -335,6 +335,27 @@ public class HashmapImplement {
         return maxLen;
     }
 
+    int majorityElement(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            if (map.get(arr[i]) > arr.length / 2) {
+                return arr[i];  // majority found
+            }
+        }
+        return -1; // if no majority element
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] arr = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         HashmapImplement hmap = new HashmapImplement();
         String s = "aabbbcc";

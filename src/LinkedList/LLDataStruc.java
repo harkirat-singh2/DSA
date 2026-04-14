61c3c07eda040432693d2f4bdc3f765be8f98ad5
Harkirat <harkiratsingh20026@gmail.com>
2026-04-14 11:20:43 +0530

    Added recursion and array DSA programs

--- src/LinkedList/LLDataStruc.java
+++ src/LinkedList/LLDataStruc.java
@@ -104,38 +104,7 @@ class Linkedlist{  // User Defined Data Structure
         size--;
     }
 
-    public Node removeNthFromEnd(Node head, int n) {
-        Node dummy = new Node(0);
-        dummy.next = head;
-        Node fast = dummy;
-        Node slow = dummy;
-        for (int i = 0; i<n;i++){
-            fast = fast.next;
-        }
-        while (fast.next!=null){
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
-         return dummy.next;
-    }
 
-    public Node oddEvenList(Node head) {
-        Node odd = head;
-        Node even = head.next;
-        Node evenHead = even;
-
-        while (even!=null && even.next!=null){
-            odd.next = even.next;
-            odd=odd.next;
-
-            even.next = odd.next;
-            even = even.next;
-        }
-        odd.next = evenHead;
-
-        return  head;
-    }
 
 }
 

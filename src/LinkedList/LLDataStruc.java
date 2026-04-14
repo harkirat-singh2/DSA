460771910aae08f520ee9149ad7103a64cece7e1
Harkirat <harkiratsingh20026@gmail.com>
2026-04-14 11:11:30 +0530

    Added recursion and array DSA programs

--- src/LinkedList/LLDataStruc.java
+++ src/LinkedList/LLDataStruc.java
@@ -125,7 +125,11 @@ class Linkedlist{  // User Defined Data Structure
         Node even = head.next;
         Node evenHead = even;
         while (even!=null && even.next!=null){
+            odd.next = even.next;
+            odd=odd.next;
 
+            even.next = odd.next;
+            even = even.next;
         }
         return  head;
     }

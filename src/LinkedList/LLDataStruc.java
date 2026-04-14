04f31fc267e09cc99bf15263e032ce9dc81cff6d
Harkirat <harkiratsingh20026@gmail.com>
2026-04-14 10:46:53 +0530

    Added recursion and array DSA programs

--- src/LinkedList/LLDataStruc.java
+++ src/LinkedList/LLDataStruc.java
@@ -103,6 +103,23 @@ class Linkedlist{  // User Defined Data Structure
         if (index==size-1) tail=temp;
         size--;
     }
+
+    public Node removeNthFromEnd(Node head, int n) {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node fast = dummy;
+        Node slow = dummy;
+        for (int i = 0; i<n;i++){
+            fast = fast.next;
+        }
+        while (fast.next!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+         return dummy.next;
+    }
+
 }
 
 

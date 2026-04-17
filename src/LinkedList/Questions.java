85a4cc08fb9999cd92d760245da8479d717687b0
Harkirat <harkiratsingh20026@gmail.com>
2026-04-17 09:13:19 +0530

    Added recursion and array DSA programs

--- src/LinkedList/Questions.java
+++ src/LinkedList/Questions.java
@@ -214,6 +214,16 @@ class Practice{
         return head;
     }
 
+    public boolean hasCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while(fast!= null && fast.next!=null){
+            fast= fast.next.next;
+            slow = slow.next;
+            if (fast==slow) return true;
+        }
+        return false;
+    }
 }
 
 public class Questions {

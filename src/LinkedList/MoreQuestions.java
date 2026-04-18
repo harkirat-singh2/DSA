cd493b90599fed502dc320dde20d9361b13885c1
Harkirat <harkiratsingh20026@gmail.com>
2026-04-18 10:57:51 +0530

    Added recursion and array DSA programs

--- src/LinkedList/MoreQuestions.java
+++ src/LinkedList/MoreQuestions.java
@@ -1,5 +1,39 @@
 package LinkedList;
 
+class Solution {
+    public Node rotateRight(Node head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        // Step 1: find length and tail
+        int n = 1;
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            n++;
+        }
+
+        // Step 2: make it circular
+        tail.next = head;
+
+        // Step 3: find new tail
+        k = k % n;
+        int stepsToNewTail = n - k;
+
+        Node newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // Step 4: break the circle
+        Node newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
+
 public class MoreQuestions {
     public Node deleteDuplicates(Node head) {
         Node curr =head;
@@ -32,9 +66,28 @@ public class MoreQuestions {
     }
 
     public Node deleteDuplicatesEntireVal(Node head) {
-
-        return head;
+        Node dummy = new Node(0);
+        Node t = dummy;
+        Node i = head;
+        while(i!=null){
+            if (i.next == null ||i.val!=i.next.val){
+                t.next=i;
+                t =i;
+                i = i.next;
+                t.next = null;
+            }
+            else {
+                Node j = i.next;
+                while (j!=null &&j.val==i.val){
+                    j= j.next;
+                }
+                i=j;
+            }
+        }
+        t.next=i;
+        return dummy.next;
     }
+    
 
     public static void main(String[] args) {
 

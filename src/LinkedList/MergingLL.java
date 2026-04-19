12321ed967b9cb50631a69e8cf620affa6053212
Harkirat <harkiratsingh20026@gmail.com>
2026-04-19 11:14:12 +0530

    Added recursion and array DSA programs

---
+++ src/LinkedList/MergingLL.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class MergingLL {
+
+    public Node mergeTwoLists(Node listA, Node listB) {
+
+        Node tempA = listA;
+        Node tempB = listB;
+        while (tempA!=null){
+            if (tempA.val <= tempB.val)
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

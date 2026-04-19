9376d3b3dd146c1c3ebbd72af46826f0b8f564b3
Harkirat <harkiratsingh20026@gmail.com>
2026-04-19 11:54:32 +0530

    Added recursion and array DSA programs

--- src/LinkedList/MergingLL.java
+++ src/LinkedList/MergingLL.java
@@ -3,13 +3,41 @@ package LinkedList;
 public class MergingLL {
 
     public Node mergeTwoLists(Node listA, Node listB) {
-
+        Node dummy = new Node(0);
+        Node t = dummy;
         Node tempA = listA;
         Node tempB = listB;
-        while (tempA!=null){
-            if (tempA.val <= tempB.val)
-        }
+        while (tempA!=null && tempB!=null){
+            if (tempA.val <= tempB.val) {
+                t.next = tempA;
+                t = t.next;
+                tempA = tempA.next;
+            }
+            else {
+                t.next = tempB;
+                t = t.next;
+                tempB = tempB.next;
+            }
 
+        }
+        if (tempA==null) t.next=tempB;
+        else t.next=tempA;
+        return dummy.next;
+    }
+
+    public Node sortList(Node head) {
+        if (head == null || head.next == null) return head;
+        Node slow = head;
+        Node fast = head;
+        while(fast.next!=null &&fast.next.next!=null){
+                slow= slow.next;
+                fast = fast.next.next;
+        }
+        Node head2 = slow.next;
+        slow.next = null;
+        Node head1 = sortList(head);
+        Node head3 = sortList(head2);
+        return mergeTwoLists(head1,head3);
     }
 
     public static void main(String[] args) {

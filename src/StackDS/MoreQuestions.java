1a6ae8747e1883a050cfbdcc4b1f14650125aea2
Harkirat <harkiratsingh20026@gmail.com>
2026-04-24 11:01:36 +0530

    Added recursion and array DSA programs

---
+++ src/StackDS/MoreQuestions.java
@@ -0,0 +1,34 @@
+package StackDS;
+
+import java.util.Stack;
+
+
+
+public class MoreQuestions {
+
+    static boolean sameStyle(char a, char b) {
+        if (a == '[' && b == ']') return true;
+        if (a == '{' && b == '}') return true;
+        return a == '(' && b == ')';
+    }
+
+    static boolean isValid(String s){
+        if (s.length()%2!=0) return false;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch=='(' || ch=='[' || ch=='{') st.push(ch);
+            else {
+                if(st.isEmpty()) return false;
+                char top = st.peek();
+                if (sameStyle(top,ch)) st.pop();
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

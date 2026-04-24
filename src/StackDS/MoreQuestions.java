9bd8c7eb4fd92ec6d71bbc55b742904cc1c5809c
Harkirat <harkiratsingh20026@gmail.com>
2026-04-24 11:15:13 +0530

    Added recursion and array DSA programs

--- src/StackDS/MoreQuestions.java
+++ src/StackDS/MoreQuestions.java
@@ -28,6 +28,24 @@ public class MoreQuestions {
         return st.isEmpty();
     }
 
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (st.isEmpty())  st.push(ch);
+            else {
+                char top = st.peek();
+                if (top == ch) st.pop();
+                else st.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+
     public static void main(String[] args) {
 
     }

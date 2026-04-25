d9ba0b621b58bd673e10b0fab1d064ad8f056a77
Harkirat <harkiratsingh20026@gmail.com>
2026-04-25 21:53:57 +0530

    Added recursion and array DSA programs

--- src/StackDS/MoreQuestions.java
+++ src/StackDS/MoreQuestions.java
@@ -95,6 +95,7 @@ public class MoreQuestions {
     public ListNode removeNodes(ListNode head){
         Stack<ListNode> st = new Stack<>();
         ListNode temp = head;
+
         while (temp!=null){
             while (!st.isEmpty() && st.peek().val < temp.val) {
                 st.pop();

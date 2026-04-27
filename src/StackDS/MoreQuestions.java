57ad94aea868759b49948c6d707f7dade3b6a972
Harkirat <harkiratsingh20026@gmail.com>
2026-04-27 22:48:30 +0530

    Added recursion and array DSA programs

--- src/StackDS/MoreQuestions.java
+++ src/StackDS/MoreQuestions.java
@@ -17,6 +17,7 @@ public class MoreQuestions {
     }
 
     static boolean isValid(String s){
+
         if (s.length()%2!=0) return false;
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {

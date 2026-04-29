6b6de6411fdcea01eb7f8169df1567f52804354a
Harkirat <harkiratsingh20026@gmail.com>
2026-04-29 08:11:14 +0530

    Added recursion and array DSA programs

--- src/StackDS/MoreQuestions.java
+++ src/StackDS/MoreQuestions.java
@@ -151,6 +151,26 @@ public class MoreQuestions {
         return ans;
     }
 
+    public int[] nextGreaterElements(int[] arr) {
+        int n= arr.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n-1; i >=0 ; i--) {
+            st.push(arr[i]);
+        }
+        for (int i = n-1; i >=0 ; i--) {
+            while (!st.isEmpty() && arr[i]>=st.peek()) st.pop();
+            if (st.isEmpty()) nge[i] = -1;
+            else nge[i] = st.peek();
+            st.push(arr[i]);
+        }
+        return nge;
+    }
+
+//    public void pattern(int n){
+//        int[] arr = new int[n];
+//
+//    }
 
 //    public ListNode removeNodesByReverse(ListNode head){
 //        head = reverseLL(head);

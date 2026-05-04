08016a7f860d6c767f26250c2d5a2c1f3525ce17
Harkirat <harkiratsingh20026@gmail.com>
2026-05-04 21:21:44 +0530

    Added recursion and array DSA programs

--- src/Rec/arrRec.java
+++ src/Rec/arrRec.java
@@ -34,6 +34,7 @@ public class arrRec {
         }
 
     public static void main(String[] args) {
+
         int[] arr= {1,2,3,4,54,6,4,3,5,3};
         System.out.println(exists(arr, 7,8));    }
 }
